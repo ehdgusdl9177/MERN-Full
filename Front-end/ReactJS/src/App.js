cf15af7d637f2ae381eafa1df55d42bd89bdb56f
ehdgusdl9177 <k23129177@gmail.com>
2024-01-24 18:29:11 +0900

    react component

---
+++ Front-end/ReactJS/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// const App = () => {
+//   return <h1>A React App!</h1>;
+// };
+
+class App extends React.Component {
+  render() {
+    return;
+    <h1 title="This works!">
+      Hi, <span>this</span> is ReactJS!
+    </h1>;
+  }
+}
+
+export default App;
