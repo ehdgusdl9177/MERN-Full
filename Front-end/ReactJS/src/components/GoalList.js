8aa59c5b28bde1d87d2d5db93a700ac438373bf0
ehdgusdl9177 <k23129177@gmail.com>
2024-01-25 10:01:33 +0900

    react component send data

--- Front-end/ReactJS/src/components/GoalList.js
+++ Front-end/ReactJS/src/components/GoalList.js
@@ -1,13 +1,8 @@
 import React from "react";
 
-const GoalList = () => {
-  return (
-    <ul className="goal-list">
-      <li>Finish the Course</li>
-      <li>Learn all about the Course Main Topic</li>
-      <li>Help other students in the Course Q&amp;A</li>
-    </ul>
-  );
+const GoalList = (props) => {
+  console.log(props.goals);
+  return <ul className="goal-list"></ul>;
 };
 
 export default GoalList;
