0134cf570d249b6fd3cd3ca66cdf5c0e050f6a01
ehdgusdl9177 <k23129177@gmail.com>
2024-01-25 11:38:05 +0900

    binding

--- Front-end/ReactJS/src/components/NewGoal/NewGoal.js
+++ Front-end/ReactJS/src/components/NewGoal/NewGoal.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewGoal.css";
+import { useState } from "react";
 
 const NewGoal = (props) => {
+  const [enteredText, setEnteredText] = useState("");
+
   const addGoalHandler = (event) => {
     event.preventDegault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: "My new goal!",
+      text: enteredText,
     };
+    setEnteredText = "";
     props.onAddGoal(newGoal);
+  };
+
+  const textChangeHandler = (event) => {
+    setEnteredText = event.target.value;
   };
 
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );
