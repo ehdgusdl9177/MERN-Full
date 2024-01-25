8ca46895bd036f9ddcc80d611eee4e9c78df4253
ehdgusdl9177 <k23129177@gmail.com>
2024-01-25 10:15:12 +0900

    data list rendering

--- Front-end/ReactJS/src/components/GoalList.js
+++ Front-end/ReactJS/src/components/GoalList.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const GoalList = (props) => {
   console.log(props.goals);
-  return <ul className="goal-list"></ul>;
+  return (
+    <ul className="goal-list">
+      {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default GoalList;
